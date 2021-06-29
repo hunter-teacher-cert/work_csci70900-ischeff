304fc0adda9c823baa9d8ae455edadcb1995a17d
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-06-29 16:16:30 -0400

    add comments/documentation

--- 01/Nim.java
+++ 01/Nim.java
@@ -17,29 +17,37 @@ public class Nim {
     Scanner in = new Scanner(System.in);
     System.out.println("This is the game of Nim. We start with 12 stones in a bag. Players choose 1, 2, or 3 stones each turn. The object of the game is to choose the last stone. \n");
 
-    while(totalStones >= 0){ // we previously had != 0, which meant that negative numbers didn't stop the loop
+    //keep playing while there are more than zero stones!
+    while(totalStones >= 0){
 
+        //solicit input from the user
         System.out.print("Player 1: How many stones would you like to choose? Choose 1, 2, or 3: ");
         playerStones = in.nextInt();
 
+        //update the total number of stones
         totalStones = totalStones - playerStones;
         System.out.println("\nRemaining stones: " + totalStones);
         System.out.println();
 
+        //check to see if the user wins
       if (totalStones <= 0) {
         System.out.println("Congrats, you won!");
         break;
       }
 
+        //generate a turn from the AI
         System.out.print("Player AI: How many stones would you like to choose? ");
         Random random = new Random();
         aiStones = random.nextInt(2) + 1;
+        //how can we have the computer NOT draw more stones than are in the bag?
         System.out.println(aiStones);
 
+        //update the total number of stones
         totalStones = totalStones - aiStones;
         System.out.println("\nRemaining stones: " + totalStones);
         System.out.println();
 
+        //check to see if the AI wins
       if (totalStones <= 0) {
         System.out.println("Sorry, the computer won!");
         break;
