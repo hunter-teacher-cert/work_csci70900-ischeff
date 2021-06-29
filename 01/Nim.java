ca1584d64cd9f1cc1ae8591bb182d4f7be7e1606
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-06-29 14:14:54 -0400

    breaking main into multiple methods

--- 01/Nim.java
+++ 01/Nim.java
@@ -18,36 +18,52 @@
  * This program was crafted by Marisa, Melissa (Best), Eric, and Ian
  */
 
-
 import java.util.Scanner; // we need to solicit user input
 import java.util.Random; // we need to generate random numbers
 
 public class Nim {
 
 /*
- * This method, modeled on the "roll" method from the craps game, will generate
- * the random number that represents the AI's "turn"
+ * The method below is modeled on the "roll" method from the craps prework.
  */
-  public static int aiTurn(int n){
-    Random random = new Random();
-    int number = random.nextInt(n) + 1; //note that we will need to set n = 2 in main
-    return number;
+ public static int aiTurn(int n){
+   Random random = new Random();
+   int number = random.nextInt(n) + 1;
+   return number;
+ }
+ /*
+  * The method below is modeled on the "roll" method from the craps prework.
+  */
+  public static int playerTurn(){ //this method will be the player's turn
+    Scanner in = new Scanner(System.in);
+    System.out.println("Player 1: How many stones would you like to choose?");
+    int playerStones = in.nextInt();
+    return playerStones;
+  }
+/*
+  public static void winCondition(int n){
+    if (n <= 0){
+      System.out.println("Congratulations, you win!");
+    }
   }
 
+  public static void round(){ //this method will run a round, and test for wins
+    int totalStones = 12;
+    int playerStones = 0;
+    int aiStones = 0;
+    playerTurn();
+
+  }
+*/
   public static void main (String[] args) {
     int totalStones = 12; //we start with twelve stones
-    Scanner in = new Scanner(System.in); //create the scanner to solicit input
     System.out.println("This is the game of Nim. We start with 12 stones in a bag.");
     System.out.println("Players choose 1, 2, or 3 stones each turn.");
     System.out.println("The object of the game is to choose the last stone.");
-//should we break here to go to another method called turn or something? 
-    System.out.println("Player 1: How many stones would you like to choose?");
-    System.out.print("Choose 1, 2, or 3: ");
-    int playerStones = in.nextInt();
-    totalStones = 12 - playerStones;
-    System.out.println("There are now " + totalStones + " stones remaining.");
-    Random random = new Random();
-    int aiStones = random.nextInt(2) + 1;
-    System.out.println(" " + aiStones);
+    System.out.println(aiTurn(2));
+    System.out.println(aiTurn(2));
+    System.out.println(aiTurn(2));
+    System.out.println(aiTurn(2));
+    playerTurn();
   }
 }
