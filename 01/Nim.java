2e74759370f0751ae6524425c4db5c7fbf63843d
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-06-29 15:49:03 -0400

    incorporated group work from repl.it and added break statement to get out of loop when player wins

--- 01/Nim.java
+++ 01/Nim.java
@@ -1,69 +1,48 @@
-/*
- * Welcome to the land of Nim! This game will test your wits against a
- * computer that will choose random numbers, and hopefully give you a run
- * for your money.
- *
- * The rules of Nim are as follows: a bag has 12 stones. You (or the AI) can
- * remove 1, 2, or 3 stones each turn from the bag. The player who removes the
- * last stone, thereby reducing the number of stones in the bag to zero, is the
- * winner. You cannot choose NOT to remove a stone on your turn.
- *
- * We have been tasked with writing a program that will...
- * a) prompt the user for how many stones (1-3) they wish to remove each turn
- * b) tell the user how many stones the AI removes and how many stones remain
- * c) keep the game going (i.e., keep soliciting input from the user and
- * generating output from the AI) until someone ones
- * d) announce the winner
- *
- * This program was crafted by Marisa, Melissa (Best), Eric, and Ian
- */
+//01/Nim.java
 
-import java.util.Scanner; // we need to solicit user input
-import java.util.Random; // we need to generate random numbers
+//Collaborators: Marisa Laks, Michelle Best, Eric Wilson, Ian Scheffler
+
+
+import java.util.Scanner;
+import java.util.Random;
+
 
 public class Nim {
 
-/*
- * The method below is modeled on the "roll" method from the craps prework.
- */
- public static int aiTurn(int n){
-   Random random = new Random();
-   int number = random.nextInt(n) + 1;
-   return number;
- }
- /*
-  * The method below is modeled on the "roll" method from the craps prework.
-  */
-  public static int playerTurn(){ //this method will be the player's turn
-    Scanner in = new Scanner(System.in);
-    System.out.println("Player 1: How many stones would you like to choose?");
-    int playerStones = in.nextInt();
-    return playerStones;
-  }
-/*
-  public static void winCondition(int n){
-    if (n <= 0){
-      System.out.println("Congratulations, you win!");
-    }
-  }
-
-  public static void round(){ //this method will run a round, and test for wins
+  public static void main (String[] args) {
     int totalStones = 12;
     int playerStones = 0;
     int aiStones = 0;
-    playerTurn();
 
-  }
-*/
-  public static void main (String[] args) {
-    int totalStones = 12; //we start with twelve stones
-    System.out.println("This is the game of Nim. We start with 12 stones in a bag.");
-    System.out.println("Players choose 1, 2, or 3 stones each turn.");
-    System.out.println("The object of the game is to choose the last stone.");
-    System.out.println(aiTurn(2));
-    System.out.println(aiTurn(2));
-    System.out.println(aiTurn(2));
-    System.out.println(aiTurn(2));
-    playerTurn();
+    Scanner in = new Scanner(System.in);
+    System.out.println("This is the game of Nim. We start with 12 stones in a bag. Players choose 1, 2, or 3 stones each turn. The object of the game is to choose the last stone. \n");
+
+    while(totalStones >= 0){ // we previously had != 0, which meant that negative numbers didn't stop the loop
+
+        System.out.print("Player 1: How many stones would you like to choose? Choose 1, 2, or 3: ");
+        playerStones = in.nextInt();
+
+        totalStones = totalStones - playerStones;
+        System.out.println("Remaining stones: " + totalStones);
+        System.out.println();
+
+      if (totalStones == 0) {
+        System.out.println("Congrats, you won!");
+        break;
+      }
+
+        System.out.print("Player AI: How many stones would you like to choose? ");
+        Random random = new Random();
+        aiStones = random.nextInt(2) + 1;
+        System.out.println(aiStones);
+
+        totalStones = totalStones - aiStones;
+        System.out.println("Remaining stones: " + totalStones);
+        System.out.println();
+
+      if (totalStones == 0) {
+        System.out.println("Sorry, the computer won!");
+      }
+    }
   }
 }
