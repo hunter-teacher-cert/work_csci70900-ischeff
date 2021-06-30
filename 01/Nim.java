a2136c60ecfffe4fe3b7e36203430559e0258929
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-06-30 13:26:46 -0400

    adjusted loop

--- 01/Nim.java
+++ 01/Nim.java
@@ -15,8 +15,8 @@ public class Nim {
     Scanner in = new Scanner(System.in);
     System.out.println("This is the game of Nim. We start with 12 stones in a bag. Players choose 1, 2, or 3 stones each turn. The object of the game is to choose the last stone. \n");
 
-    //loop to keep playing while there are more than zero stones!
-    while(totalStones >= 0){
+    //loop to keep playing until the break statements are reached. 
+    while(true){
 
         //solicit input from the user
         System.out.print("Player 1: How many stones would you like to choose? Choose 1, 2, or 3: ");
@@ -38,6 +38,7 @@ public class Nim {
         Random random = new Random();
         aiStones = random.nextInt(3) + 1; //fixed this based on Liam and Peter's example
         //how can we have the computer NOT draw more stones than are in the bag?
+        //maybe we should use Peter and Liam's idea of optimizing the AI's choice at this point?
         System.out.println(aiStones);
 
         //update the total number of stones
