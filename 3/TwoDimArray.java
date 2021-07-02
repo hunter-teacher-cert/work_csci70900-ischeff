e98b615f3f46e8ec7db53191a69614a262df9f75
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-07-01 23:31:26 -0400

    isolating array out of bounds exception

--- 3/TwoDimArray.java
+++ 3/TwoDimArray.java
@@ -94,9 +94,9 @@ public class TwoDimArray
        System.out.println("sum m2 : " + sum1(m2));
        System.out.println("sum m3 : " + sum1(m3));
        System.out.print("testing sum2...\n");
-       System.out.println("sum m1 : " + sum2(m1));
-       System.out.println("sum m2 : " + sum2(m2));
-       System.out.println("sum m3 : " + sum2(m3));
+       System.out.println("sum m1 : " + sum2(m1));//this seems to work with your current sum2--why not the other tests?
+       //System.out.println("sum m2 : " + sum2(m2));//these tests are causing sum2 issues--array out of bounds!
+       //System.out.println("sum m3 : " + sum2(m3));
        /* ~~~v~~~~~slide~me~down~as~you~test~~~~~~~~~~~~~~~v~~~
        ~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~*/
   }//end main()
