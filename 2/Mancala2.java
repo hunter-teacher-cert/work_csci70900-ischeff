34d93baa280a81cba07f82c9bc18d1fa4493a5e3
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-07-03 16:36:43 -0400

    expanding skeleton comments to plan methods

--- 2/Mancala2.java
+++ 2/Mancala2.java
@@ -21,6 +21,28 @@ public class Mancala2{
     System.out.print(" | {" + a[6] + "}" + "\n" + "\n");//player 1's mancala
   }//end of printBoard
 
+  //method to solicit player one's input
+  public static int playerOneInput(){
+    //Create loop to check user input is valid (shoutout to Emma Wingreen!)
+    do{
+      //solicit user input
+      System.out.println("Player 1, from which pit would you like to pick stones?\n");
+      //collect the user input
+      pit = in.nextInt();
+      //error message for invalid user input (invalid pit)
+      if (pit < 0 || pit > 5){
+        System.out.println("Remember, the pits are numbered 0 to 5! Try again!\n");
+      }
+      //error message for invalid user input (no stones in pit)
+      if (board[pit] == 0){
+        System.out.println("You can't pick a pit without stones! Try again!\n");
+        playerOneInput();
+      }
+    } while(pit < 0 || pit > 5);
+    //System.out.println(pit);
+    return pit;
+  } //end of playerInput
+
   public static void main(String[] args){
     //Test that the program compiles
     System.out.println("This will be a game of Mancala!");
@@ -29,16 +51,27 @@ public class Mancala2{
     //Test the mancala variables are assigned correctly
     System.out.println(player1Mancala);//should be zero at start
     System.out.println(player2Mancala);//should be zero at start
-    printBoard(board);
+    printBoard(board);//print the starting board
 
-    //print the board (with a for loop)
+    //while loop as long as gameOver conditions haven't been met
+      //gameOver conditions: one player's side has no more stones in the pits.
 
-    //while loop as long as win conditions haven't been met
+      //player 1 turn
+        //get player 1 input
+          //check if input is valid
+            //if not valid, ask for input again
+        //set the value of stones in the players' "hand" equal to the number of stones in the pit they selected.
+        //increment the array based on the number of stones.
+          //note: can you make a general mancala algorithm that moves stones, and just takes the board and n as an input?
+        //check if player 1's last stone went into their Mancala
+          //if so, player 1's turn repeats
+        //check if player 1's last stone went into an empty pit on their side
+          //if so, check if parallel player2 pit has any stones
+            //if so,
+        //check if player 1 wins
 
-      //player turn
-        //check if player wins
-
-      //ai turn
+      //player 2 turn
+        //check if player 2 wins
 
     //print winning statement/final scores?
 
