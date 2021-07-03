b8c848e709d8080f0b6e536f235544480f9ab5b5
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-07-03 16:28:01 -0400

    testing printBoard method

--- 2/Mancala2.java
+++ 2/Mancala2.java
@@ -6,6 +6,21 @@ public class Mancala2{
   public static int player1Mancala = board[6];
   public static int player2Mancala = board[13];
 
+  //method to print the board by looping through board array
+  public static void printBoard(int[] a) {
+    System.out.print("\t{" + a[13] + "}"); //player2's mancala
+    for (int i = 12; i > 6; i--) { //player 2's pits
+        System.out.print(" | " + a[i]);
+    }
+    System.out.print(" |");
+    System.out.println("");
+    System.out.print("\t    | " + a[0]);//player 1's pits
+    for (int i = 1; i < 6; i++) {
+        System.out.print(" | " + a[i]);
+    }
+    System.out.print(" | {" + a[6] + "}" + "\n" + "\n");//player 1's mancala
+  }//end of printBoard
+
   public static void main(String[] args){
     //Test that the program compiles
     System.out.println("This will be a game of Mancala!");
@@ -14,8 +29,7 @@ public class Mancala2{
     //Test the mancala variables are assigned correctly
     System.out.println(player1Mancala);//should be zero at start
     System.out.println(player2Mancala);//should be zero at start
-
-    //create the mancala board (as a single 1-D array)
+    printBoard(board);
 
     //print the board (with a for loop)
 
