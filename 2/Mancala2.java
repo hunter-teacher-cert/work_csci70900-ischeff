55331af56830a21400cf10f4d74db4c58dfbed1f
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-07-04 20:26:09 -0400

    fixed p2 input

--- 2/Mancala2.java
+++ 2/Mancala2.java
@@ -27,6 +27,8 @@ public class Mancala2{
         System.out.print(" | " + a[i]);
     }
     System.out.print(" | {" + a[6] + "}" + "\n" + "\n");//player 1's mancala
+    System.out.println("Player 1 Mancala: " + p1Mancala);
+    System.out.println("Player 2 Mancala: " + p2Mancala + "\n");
   }//end of printBoard
 
   //method to check if game is over (will return true if game is over)
@@ -50,20 +52,55 @@ public class Mancala2{
       return p1PitChoice;
     }
     return 0;
-  }//end of player input
+  }//end of player 1 input
 
   //this method will move player 1's stones around the board.
   public static void moveP1Stones(int p1PitChoice){
-    System.out.println("This method will move stones on the board.");
+    System.out.println("This method will move stones on the board.");//test
     int pit = p1PitChoice;
-    System.out.println(p1PitChoice);
+    System.out.println(p1PitChoice);//test
     int stonesInHand = board[p1PitChoice];
-    System.out.println(stonesInHand);
+    System.out.println(stonesInHand);//test
     board[p1PitChoice] = 0;
     for(int i = 1; i <= stonesInHand; i++){
       board[pit + i]++;
     }//end of for loop
-    printBoard(board);
+    p1Mancala = board[6];
+    p2Mancala = board[13];
+    printBoard(board);//test
+  }//end of moveP1Stones
+
+  //get input from player 1
+  public static int p2Input(){
+    System.out.println("Player 2: input a number between 1 and 6.");
+    p2PitChoice = (in.nextInt() + 6);
+    if (p2PitChoice < 7 || p2PitChoice > 12){//check if pit exists
+      System.out.println("Sorry, pick a pit between 1 and 6!");
+      p2Input();
+    } else if (board[p2PitChoice] == 0){//check if pit has stones
+      System.out.println("Sorry, pick a pit with stones in it!");
+      p2Input();
+    } else{
+      System.out.println(p2PitChoice);//test
+      return p2PitChoice;
+    }
+    return 0;
+  }//end of player 2 input
+
+  //this method will move player 1's stones around the board.
+  public static void moveP2Stones(int p2PitChoice){
+    System.out.println("This method will move stones on the board.");//test
+    int pit = p2PitChoice;
+    System.out.println(p2PitChoice);//test
+    int stonesInHand = board[p2PitChoice];
+    System.out.println(stonesInHand);//test
+    board[p2PitChoice] = 0;
+    for(int i = 1; i <= stonesInHand; i++){
+      board[pit + i]++;
+    }//end of for loop
+    p1Mancala = board[6];
+    p2Mancala = board[13];
+    printBoard(board);//test
   }//end of moveP1Stones
 
   public static void main(String[] args){
@@ -72,35 +109,35 @@ public class Mancala2{
     //Test that the gameboard array was created; expect a hexadecimal address
     System.out.println(board);
     //Test the mancala variables are assigned correctly
-    System.out.println(p1Mancala);//should be zero at start
-    System.out.println(p2Mancala);//should be zero at start
+    //System.out.println(p1Mancala);//should be zero at start
+    //System.out.println(p2Mancala);//should be zero at start
     printBoard(board);//print the starting board
-    System.out.println(isGameOver());//test if game is over
+    //System.out.println(isGameOver());//test if game is over
 
     //loop for gameplay (will break when isGameOver evaluates to true in body)
     while(true){
-      printBoard(board);
-      p1Input();//think this works now!
-      moveP1Stones(p1PitChoice);
+      //printBoard(board);
+      //p1Input();//get input from player 1
+      //moveP1Stones(p1PitChoice); //move player 1's stones around the board
+        //add in adjustment if you need to loop around.
+      //check if player 1's last stone went into their Mancala
+        //if so, player 1's turn repeats
+      //check if player 1's last stone went into an empty pit on their side
+        //if so, check if parallel player2 pit has any stones
+          //if so, zero out that p2 pit and the p1 pit that is parallel
+            //add sum of stones to p1 mancala
+      //check if game is over
+        //if so, break
+      p2Input();
+      moveP2Stones(p2PitChoice);
       break;
-      //player 1 turn
-        //set the value of stones in the players' "hand" equal to the number of stones in the pit they selected.
-        //increment the array based on the number of stones.
-          //note: can you make a general mancala algorithm that moves stones, and just takes the board and n as an input?
-        //check if player 1's last stone went into their Mancala
-          //if so, player 1's turn repeats
-        //check if player 1's last stone went into an empty pit on their side
-          //if so, check if parallel player2 pit has any stones
-            //if so,
-        //check if player 1 wins
 
-      //player 2 turn
-        //check if player 2 wins
   }//end of gameplay while loop
 
     //add any remainig stones in player 1 pits to their Mancala
     //add any remaining stones in player 2 pits to their mancala
     //check to see who won
+      //if no one wins, it's a tie
     //print winning statement/final scores?
 
   }//end of main
