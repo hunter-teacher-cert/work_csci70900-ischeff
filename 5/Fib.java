4c391659431e3978bef67060afd719ede43acbd1
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-07-06 10:50:13 -0400

    Added loop to show more numbers in the sequence

--- 5/Fib.java
+++ 5/Fib.java
@@ -1,6 +1,9 @@
 //This program was a collaboration among Eric Wilson, Dwayne Levene, and Ian Scheffler
+import java.io.*;
+import java.util.*;
+
 class Fib{
-  public static int fibLoop (int n){
+  public static int fibSeq (int n){
     if (n < 0){
       System.out.println("Only positive inputs please!");
     }
@@ -11,13 +14,21 @@ class Fib{
       return 1;
     }
     if (n > 1){
-      return (fibLoop(n-1) + fibLoop(n-2));
+      return (fibSeq(n-1) + fibSeq(n-2));
     }
     return 0;
+  }//end of fibSeq
+
+  public static void fibLoop(int n){
+    int result = 0;
+    for(int i = 0; i < n; i++){
+      System.out.println("Term: " + i + " | Value: " + fibSeq(i));
+    }
   }//end of fibLoop
 
   public static void main(String[] args) {
-      System.out.println("Hello world!");
-      System.out.println(fibLoop(10));
+      System.out.println("This program will recursively implement the Fibonacci sequence!");
+      System.out.println(fibSeq(10));
+      fibLoop(100);
     }//end main
 }//end of class
