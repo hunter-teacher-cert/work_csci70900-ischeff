bed6af105802090bf44251883a8dcd9ccce32a7f
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-07-06 11:53:02 -0400

    added to-do list

--- 5/Driver.java
+++ 5/Driver.java
@@ -1,3 +1,16 @@
+//This program was a collaboration among Eric Wilson, Dwayne Levene, and Ian Scheffler
+
+/**
+Your driver file should contain code to (in suggested development order)
+1) instantiate a newly-created Time object (aka "instance of class Time") with default value(s)
+2) instantiate a newly-created Time object with specified value(s)
+3) assign a Time var the value null
+4) assign a Time var the value of an existing Time var
+5) print all of the above (See multiple ways to do this? Try all -- and document in comments!)
+6) test for equality of each of the Time instances above. Print results.
+7) add two Time objects and print results
+8) add two Time objects and save the result in another Time object
+*/
 import java.io.*;
 import java.util.*;
 
--- 5/Time.java
+++ 5/Time.java
@@ -1,3 +1,9 @@
+//This program was a collaboration among Eric Wilson, Dwayne Levene, and Ian Scheffler
+
+/**
+* Your Time class definition file should contain code to:
+* tell a Time object how to do the tasks necessary to achieve the above
+*/
 import java.io.*;
 import java.util.*;
 
