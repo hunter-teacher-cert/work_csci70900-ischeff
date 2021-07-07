0293c52a5df56ea56e13f0ccff43efe1a14d6325
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-07-07 11:58:24 -0400

    copied over groupwork from teletype

--- 6/SuperArray.java
+++ 6/SuperArray.java
@@ -1,6 +1,12 @@
+// Group 0
+// Ian Scheffler
+// Brian Mueller
+// Jovani Cardenas
+
 /**
    lab skeleton
    encapsulation / SuperArray
+
    SuperArray is a wrapper class for an array.
    Provides "pass-thru" accessibility to array capabilities:
    - get/set by index
@@ -21,49 +27,70 @@ public class SuperArray
 
 
   // ~~~~~~~~~~~~~~~ CONSTRUCTORS ~~~~~~~~~~~~~~~
-  //overloaded constructor -- allows specification of initial capacity
-  public SuperArray( int size )
-  {
-
-  }
 
   //default constructor -- initializes capacity to 10
   public SuperArray()
   {
-
+    this.data = new int[10];
+    this.numberElements = 0;
   }
 
+  //overloaded constructor -- allows specification of initial capacity
+  public SuperArray( int size )
+  {
+    this.data = new int[size];
+    this.numberElements = 0;
+  }
 
   // ~~~~~~~~~~~~~~~ METHODS ~~~~~~~~~~~~~~~
   public void add( int value )
   {
+    //default constructor - Number of Elements is zero.
     // test to see if we need to grow, then grow
     // SIMPLE VERSION DOES NOT AUTO-GROW CAPACITY; INSERT MORE CODE HERE LATER
-
+    if (numberElements == data.length){
+      grow();
+    }
     // add item
-
+    data[numberElements] = value;
 
     // increment numberElements
-
+    numberElements++;
 
   }//end add()
 
-
-  public boolean isEmpty()
-  {
+  // overloaded method that allows for insertion
+  public void add(int index, int value){
 
   }
+
+  public boolean isEmpty()//we are defining empty as a set of all zeroes.
+  {
+    for (int i= 0; i<data.length; i++) {
+      // if the current element is NOT 0, return false (will exit method early)
+      if (data[i] != 0){
+        return false;
+      }
+    }
+    return true;
+  }//end of isEmpty
 
 
   public int get(int index)
   {
-
+    return 0;
   }
 
-
+  // example: {0,1,2,3,4,0,0,0,0,0}
+  // note: tofr swung by and suggested we further discuss what we want toString to print
+  //i.e., do we want to print only meaningful data?
   public String toString()
   {
-
+    String s = "{";
+    for(int i = 0; i < data.length; i++){
+      s += data[i] + ","; // TODO: remove trailing comma at end
+    }
+    return s+"}";
   }//end toString()
 
 
@@ -84,11 +111,16 @@ public class SuperArray
   private void grow()
   {
     // create a new array with extra space
-    // Q: How did you decide how much to increase capacity by?
+    // Q: How did you decide how much to increase capacity by? we chose +10
+    int[] biggerData = new int[numberElements+10];
 
     // copy over all the elements from the old array to the new one
+    for(int i = 0; i < data.length; i++) {
+      biggerData[i] = data[i];
+    }
 
     // point data to the new array
+    data = biggerData;
     // Q: How does this look when illustrated using encapsulation diagram?
 
   }//end grow()
