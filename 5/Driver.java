681601d15d40394c3dc12cce9f9ff41856742248
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-07-06 23:55:26 -0400

    added tests

--- 5/Driver.java
+++ 5/Driver.java
@@ -25,6 +25,7 @@ public class Driver {
     //5) print all of the above (See multiple ways to do this? Try all -- and document in comments!)
 
     //this first round of printing uses the method printTime from the Time class
+    System.out.println("Testing the printTime method:");
     t1.printTime(t1);//should equal 00:00:00.0
     t2.printTime(t2);//should equal 00:00:00.0
     t3.printTime(t3);//should equal 06:07:30.0
@@ -34,6 +35,7 @@ public class Driver {
     //this second round of printing use the .toString method
     //First, we instantiate string variables to hold the resulting strings.
     //The values should be the same as the printTime method above.
+    System.out.println("Testing the .toString method:");
     String s1 = t1.toString();//should equal 00:00:00.0
     String s2 = t2.toString();//should equal 00:00:00.0
     String s3 = t3.toString();//should equal 06:07:30.0
@@ -41,13 +43,23 @@ public class Driver {
     String s5 = t5.toString();//should equal 06:07:30.0
 
     //The statements below will then print the strings.
-    System.out.println(s1);
-    System.out.println(s2);
-    System.out.println(s3);
+    System.out.println("actual: " + s1 + " | expected: 00:00:00.0");
+    System.out.println("actual: " + s2 + " | expected: 00:00:00.0");
+    System.out.println("actual: " + s3 + " | expected: 06:07:30.0");
     //System.out.println(s4);
-    System.out.println(s5);
-
-
+    System.out.println("actual: " + s5 + " | expected: 06:07:30.0");
+    System.out.println();//linebreak
+    //checking equivalence using the equals method
+    System.out.println("Testing the equals method:");
+    System.out.println("actual: " + t1.equals(t2) + " | expected: true"); //true
+    System.out.println("actual: " + t1.equals(t3) + " | expected: false"); //false
+    System.out.println("actual: " + t1.equals(t5) + " | expected: false"); //false
+    System.out.println("actual: " + t2.equals(t1) + " | expected: true"); //true
+    System.out.println("actual: " + t2.equals(t3) + " | expected: false"); //false
+    System.out.println("actual: " + t2.equals(t5) + " | expected: false"); //false
+    System.out.println("actual: " + t5.equals(t1) + " | expected: false"); //false
+    System.out.println("actual: " + t5.equals(t2) + " | expected: false"); //false
+    System.out.println("actual: " + t5.equals(t3) + " | expected: true"); //true
 
   }//end main
 
