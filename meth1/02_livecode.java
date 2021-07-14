6c39c50051cf2b231564e10be06fc4024f3bd9aa
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-07-14 08:57:41 -0400

    added comments

---
+++ meth1/02_livecode.java
@@ -0,0 +1,18 @@
+//How should I start?
+public class Hello { //start writing a print Hello world statement withOUT declaring the class; see if anyone notices; if not, circle back when it doesn't compile
+  public static void main(String[] args){//again, just start with a print statement withOUT any of this at first if no one brings it up.
+    System.out.println("Hello world!");//What would happen if I remove the quotes? NOTE: don't add semicolon at first. Maybe also don't add quotes.
+  }
+  System.out.println("Hello world, again!"); //now, ask for input on creating another print statement--can be different from this--but put it OUTSIDE the main method; see if anyone notices.
+} //Don't include and/or misplace the brackets and see if anyone notices.
+
+//as errors come up--and there will be a LOT of compilation errors, make sure to read through each one with the class and get their thoguhts on what is going on/what should change.
+
+/*
+A brief list of expected errors:
+- class not declared (when you just start with print statement)
+- no main method (when you just start with print statement)
+- print error (when string isn't formatted correctly)
+- lacking semicolon
+- error that class and filename aren't the same 
+*/
