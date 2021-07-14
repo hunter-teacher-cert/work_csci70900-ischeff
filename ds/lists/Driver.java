03d82e2afdceff34da926985c04e14bace46fb8a
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-07-14 14:37:54 -0400

    created and manipulated a linked list

--- ds/lists/Driver.java
+++ ds/lists/Driver.java
@@ -3,7 +3,8 @@ import java.util.*;
 
 public class Driver{
     public static void main(String[] args) {
-	Node n;
+/*
+  Node n;
 	n = new Node();
 	n.setData("Eduardo");
 
@@ -19,14 +20,61 @@ public class Driver{
 	n.getNext().setNext(new Node("Steph"));
 	System.out.println(n2.getNext());
 	System.out.println(n.getNext().getNext());
+*/
 
-	Node L;
-	// 1.Create a new  list that looks like:
-	//   L->"a"->"b"->"c"->"d"
-	// 2. Write the code to insert an "x"
-	//    between the b and the c
-	// 3. Write the code to delete the c
+// 1.Create a new  list that looks like:
+//   L->"a"->"b"->"c"->"d"
+	Node L; //L points to a node
+  Node a = new Node("a");//this line and the next three lines
+  Node b = new Node("b");
+  Node c = new Node("c");
+  Node d = new Node("d");
 
 
-    }
-}
+  //the code below links together the nodes
+  L = a;
+  a.setNext(b);
+  b.setNext(c);
+  c.setNext(d);
+  //test print
+  while (L != null){
+    System.out.print(L + " --> ");
+    L = L.getNext();
+  }
+  System.out.println("null");
+
+  //L --> a --> b --> c --> d --> Null
+
+  // 2. Write the code to insert an "x"
+  //    between the b and the c
+  Node x = new Node("x");
+  x.setNext(c);
+  b.setNext(x);
+
+  //L --> a --> b --> x --> c --> d --> Null
+
+  // 3. Write the code to delete the c
+  b.setNext(d);
+
+
+
+  //L.setNext(new Node("b"));
+  //L.setNext(new Node("b")).setNext(new Node("c").setNext(new Node("d")));
+  // L.getNext().setNext(new Node("b"));
+  // System.out.println(L);
+  // System.out.println(L.getNext());
+  // System.out.println(L.getNext().getNext());
+  // System.out.println(L.getNext().getNext());
+  // L.getNext().setNext(new Node("c"));
+  // System.out.println(L);
+  // System.out.println(L.getNext().getNext());
+
+
+
+
+
+//Note: the below is based on Brian, Lyuba, and Mamudu's code, as well as Mike's code:
+
+
+  }//end of main
+}//end of class
