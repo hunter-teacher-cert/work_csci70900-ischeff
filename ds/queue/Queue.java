5157a179c737d3c06ca7dd62b72da752a36ee4d0
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-07-21 15:12:50 -0400

    added skeleton comments and instance variables

--- ds/queue/Queue.java
+++ ds/queue/Queue.java
@@ -1,3 +1,23 @@
 public class Queue{
 
+  //instance Variables
+  private Node head; //the start of the queue --> where nodes are removed
+  private Node tail; //the end of the queue --> where nodes are added
+
+  //make constructors
+    //default constructor - null?
+
+  //make value constructor
+    //what should it include?
+
+  // enqueue(value) <-- add value to the end of the queue
+
+  // x = dequeue() <-- remove and return the value at the front of the queue
+
+  // x = front() <-- return but don't remove the value at the front
+
+  // isEmpty() <-- returns true if the queue is empty
+
+  // size() <-- returns the number of items in the queue
+
 }//end of class
