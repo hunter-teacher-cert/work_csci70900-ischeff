68c9b9268535d2353539fc5100e260cbb8e86130
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-07-21 19:00:19 -0400

    tested enqueue

--- ds/queue/Driver.java
+++ ds/queue/Driver.java
@@ -1,8 +1,14 @@
-/*
-This file will be used to direct/drive the Queue class file
-*/
 public class Driver{
   public static void main(String[] args){
     //System.out.println("Hello, world!");
+    Queue queue = new Queue();
+    System.out.println(queue);//should print out null
+    queue.enqueue("Apple");
+    System.out.println(queue);//should print out "Apple --> null"
+    queue.enqueue("Banana");
+    System.out.println(queue);//should print out "Apple --> Banana --> null"
+    queue.enqueue("Pear");
+    System.out.println(queue);//should print out "Apple --> Banana --> Pear --> null "
+
   }//end of main
 }//end of class
