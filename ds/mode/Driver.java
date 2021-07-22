4c01f17d2dbb59d9c943285072e15565d61e4466
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-07-22 11:11:49 -0400

    added runtime tests

--- ds/mode/Driver.java
+++ ds/mode/Driver.java
@@ -4,10 +4,13 @@ import java.util.*;
 public class Driver{
 
   public static void main(String[] args) {
-  	Mode m = new Mode();
-  	System.out.println(m);
-    m.allFreq();
-    System.out.println();
-    System.out.println("find mode: "+ m.calcMode());
+  	Mode m = new Mode(100000);
+  	//System.out.println(m);
+    //m.allFreq();
+    //System.out.println();
+    long start = System.currentTimeMillis();
+    System.out.println("find mode: "+m.calcMode());
+    long end = System.currentTimeMillis() -start;
+    System.out.println(end+" ms");
   }
-}//end of class
+}
