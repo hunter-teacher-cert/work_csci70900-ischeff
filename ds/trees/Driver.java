72b258276f1766f99394c68e39ad41a2c18bb119
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-07-25 21:26:40 -0400

    Added search test code

--- ds/trees/Driver.java
+++ ds/trees/Driver.java
@@ -4,24 +4,24 @@ import java.util.*;
 
 public class Driver {
     public static void main(String[] args) {
-	    BSTree t = new BSTree();
+      BSTree t = new BSTree();
       //create tree of specified design
-  		t.seed();
+      t.seed();
       //create an int to hold values
-  		int value;
+      int value;
       //search for 10 in the tree (should be there--it's the root!)
       value = t.search(10);
-  		System.out.println(value);
+      System.out.println(value);
       //search for 15 in the tree (should be there)
       value = t.search(15);
-  		System.out.println(value);
+      System.out.println(value);
       //search for 17 in tree (should NOT be there)
       try {
-  			value = t.search(17);
-  			System.out.println(value);
-  		} catch (NullPointerException e) {
-  			System.out.println("17 not in tree");
-  		}
+        value = t.search(17);
+        System.out.println(value);
+      } catch (NullPointerException e) {
+        System.out.println("17 not in tree");
+      }
 
     }//end of main
 }//end of class
