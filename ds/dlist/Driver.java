2fc6aa2107822f0825582840978ff33d54dbdc09
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-07-26 13:29:34 -0400

    updating with code to modify from Llist

---
+++ ds/dlist/Driver.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+  public static void main(String[] args){
+    System.out.println("Hello, world!");
+    Node d = new Node();
+    System.out.println(d);
+;  }//end of main
+}//end of class
