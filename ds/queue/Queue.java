f9f0e156c3e06b4e1e365b77dfa656703365d4ed
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-07-29 11:33:06 -0400

    revised constructor

--- ds/queue/Queue.java
+++ ds/queue/Queue.java
@@ -3,11 +3,15 @@ public class Queue{
   //instance Variables
   private Node head; //the start of the queue --> where nodes are removed
   private Node tail; //the end of the queue --> where nodes are added
+  private int size; //the size (length) of the queue
 
-  //make constructors --> head points to tail? both are null?
+  /**
+  * This is the default constructor;
+  */
   public Queue(){
     head = null;
     tail = null;
+    size = 0;
   }
   //make value constructor
     //what should it include?
@@ -31,9 +35,11 @@ public class Queue{
     if (head == null){
       head = newNode;
       tail = newNode;
+      size++;
     } else {
       tail.setNext(newNode);
       tail = newNode;
+      size++;
     }
   }//end of enqueue
 
@@ -41,18 +47,20 @@ public class Queue{
   public String dequeue(){
     Node currentNode = head;
     if (head == null){
-      return null;
+      throw new NullPointerException("Queue is empty!");
     }
     head = currentNode.getNext();
     return currentNode.getData();
+    size--;
   }
 
   // x = front() <-- return but don't remove the value at the front
   public String head(){
-    if(head == null){
-      return null;
+    if(!this.isEmpty()){
+      return head.getData();
+    } else {
+      throw new NullPointerException("Queue is empty!");
     }
-    return head.getData();
   }
 
   // isEmpty() <-- returns true if the queue is empty
@@ -62,13 +70,14 @@ public class Queue{
 
   // size() <-- returns the number of items in the queue
   public int size(){
-    Node currentNode = head;
-    int counter = 0;
-    while (currentNode != null){
-        counter++;
-        currentNode = currentNode.getNext();
-    }
-    return counter;
+    return size;
+    // Node currentNode = head;
+    // int counter = 0;
+    // while (currentNode != null){
+    //     counter++;
+    //     currentNode = currentNode.getNext();
+    // }
+    // return counter;
   }
 
 }//end of class
