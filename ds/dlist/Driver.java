40a3444a791a455fb28873588f9243df7e4e6739
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-07-30 11:57:54 -0400

    added tests

--- ds/dlist/Driver.java
+++ ds/dlist/Driver.java
@@ -49,219 +49,29 @@ public class Driver{
     System.out.println("At what index is Plum? " + d.search("Plum"));
     // d.set(-15, "Plum");//should throw an exception
     // d.set(25, "Plum");//should throw an exception
-    // System.out.println(d);
-  //   d.insert(0, "Apple");
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println(d);
-  //   System.out.println();
-  //   d.addFront("Peach");
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println(d);
-  //   System.out.println();
-  //   d.insert(1, "Banana");
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println(d);
-  //   System.out.println();
-  //   d.remove(2);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println(d);
-  //   System.out.println();
-  //   d.addTail("Kumquat");
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println(d);
-  //   System.out.println("0th index is: " + d.get(0));
-  //   System.out.println("1st index is: " + d.get(1));
-  //   System.out.println("2nd index is: " + d.get(2));
-  //   try{
-  //   System.out.println("3rd index is: " + d.get(3));
-  // } catch (Exception e){
-  //   System.out.println("Not a valid index!");
-  // }
-  //   try{
-  //   System.out.println("-1st index is: " + d.get(-1));
-  // } catch (Exception e){
-  //   System.out.println("Not a valid index!");
-  // }
-  // // System.out.println(d.get(0));
-  //   // System.out.println(d.get(1));
-  //   // System.out.println(d.get(2));
-  //   // System.out.println(d);
-  // //   System.out.println("Length: " + d.length());
-  // //   try{
-  // //
-  // // } catch (Exception e){
-  // //   System.out.println("Null pointer exception");
-  // // }
-    // d.addFront("Pear");
-    // System.out.println("Length: " + d.length());
-    // System.out.println("IsEmpty: " + d.isEmpty());
-    //  System.out.println(d);
-  //   d.addFront("Pear");
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println();
-  //   d.addFront("Banana");
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println();
-  //   System.out.println(d.get(0));
-  //   System.out.println(d.get(1));
-  //   // System.out.println(d.get(2));
-  //   // System.out.println(d.get(3));
-  //   // System.out.println(d.get(-1));
-  //   System.out.println();
-  //   d.set(0, "Pear");
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println();
-  //   d.set(1, "Peach");
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println();
-  //   d.set(0, "Pear");
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println();
-  //   try{
-  //     d.set(-1, "Peach");
-  //   } catch (Exception e){
-  //     System.out.println(e);
-  //     System.out.println(d);
-  //     System.out.println("IsEmpty: " + d.isEmpty());
-  //     System.out.println("Length: " + d.length());
-  //   }
-  //   System.out.println();
-  //   d.insert(1, "Pineapple");
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println();
-  //   d.insert(1, "Starfruit");
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println();
-  //   d.insert(2, "Durian");
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println();
-  //   d.insert(0, "Crabapple");
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println();
-  //   try{
-  //   d.insert(5, "Blackberry");
-  // } catch (Exception e){
-  //   System.out.println(e);
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  // }
-  //   System.out.println();
-  //
-  //   try{
-  //   d.insert(10, "Raspberry");
-  // } catch (Exception e){
-  //   System.out.println(e);
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  // }
-  //   System.out.println();
-  //   try{
-  //   d.insert(7, "Raspberry");
-  // } catch (Exception e){
-  //   System.out.println(e);
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println();
-  // }
-  //   System.out.println(d.search("Pineapple"));
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println(d.search("Pear"));
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println(d.search("Watermelon"));
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println();
-  //   System.out.println("Tests for remove:");
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println();
-  //   d.remove(2);
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println();
-  //   d.remove(2);
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println();
-  //   // d.remove(0);
-  //   // System.out.println(d);
-  //   // System.out.println("IsEmpty: " + d.isEmpty());
-  //   // System.out.println("Length: " + d.length());
-  //   // System.out.println();
-  //   d.remove(6);
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println();
-  //   d.remove(5);
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println();
-  //   d.remove(4);
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println();
-  //   d.remove(3);
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println();
-  //   d.remove(2);
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println();
-  //   d.remove(1);
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println();
-  //   d.remove(0);
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println();
-  //   d.remove(0);
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
-  //   System.out.println();
-  //   d.remove(-1);
-  //   System.out.println(d);
-  //   System.out.println("IsEmpty: " + d.isEmpty());
-  //   System.out.println("Length: " + d.length());
+    System.out.println();
+    d.remove(2);
+    System.out.println(d);
+    System.out.println("IsEmpty: " + d.isEmpty());
+    System.out.println("Length: " + d.length());
+    System.out.println("Front: " + d.getFront());
+    System.out.println("Tail: " + d.getTail());
+    System.out.println();
+    // d.remove(2);//should throw an exception
+    // d.remove(-1); //should throw an exception
+    // d.remove(25); //should throw an exception
+    d.remove(1);
+    System.out.println(d);
+    System.out.println("IsEmpty: " + d.isEmpty());
+    System.out.println("Length: " + d.length());
+    System.out.println("Front: " + d.getFront());
+    System.out.println("Tail: " + d.getTail());
+    System.out.println();
+    d.remove(0);
+    System.out.println(d);
+    System.out.println("IsEmpty: " + d.isEmpty());
+    System.out.println("Length: " + d.length());
+    System.out.println("Front: " + d.getFront());
+    System.out.println("Tail: " + d.getTail());
   }//end of main
 }//end of class
