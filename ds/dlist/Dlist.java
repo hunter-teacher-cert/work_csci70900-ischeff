19f773bff41fa2b0a0684d6ac9ca6555b3b1a0c9
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-07-30 01:31:24 -0400

    added comments, revised reverse traversal

--- ds/dlist/Dlist.java
+++ ds/dlist/Dlist.java
@@ -103,6 +103,7 @@ public class Dlist{
   * If the given index is closer to tail, method starts at tail and traverses in reverse to find the index.
   * If the given index is closer to head, method starts at head and traverses forward to find the index.
   * Once the index is found, the method returns the data contained in the node at the given index.
+  * If the index is not in bounds, will throw a NullPointerException.
   *
   * @param index - the index from which to return the node's data.
   *
@@ -141,6 +142,7 @@ public class Dlist{
   * If the given index is closer to tail, method starts at tail and traverses in reverse to find the index.
   * If the given index is closer to head, method starts at head and traverses forward to find the index.
   * Once the index is found, the method sets the data contained in the node at the given index to the data in the parameter value.
+  * If the index is not in bounds, will throw a NullPointerException.
   *
   * @param index - the index at which to set the node's data.
   * @param value - the string with which to set the data of the node at the given index.
@@ -148,17 +150,19 @@ public class Dlist{
   * @return void
   */
   public void set(int index, String value){
-    if(index > size / 2){
-      Node currentNode = tail.getPrev();
-      int counter = size - 1;
-      while (currentNode != front){
-        if (counter == index){
-          currentNode.setData(value);
-          break;
+    if(index > size || index < 0){
+      throw new NullPointerException("Not a valid index!");
+    } else if(index > size / 2){
+        Node currentNode = tail.getPrev();
+        int counter = size - 1;
+        while (currentNode != front){
+          if (counter == index){
+            currentNode.setData(value);
+            break;
+          }
+          counter--;
+          currentNode = currentNode.getPrev();
         }
-        counter--;
-        currentNode = currentNode.getPrev();
-      }
     } else {
       Node currentNode = front.getNext();
       int counter = 0;
@@ -173,8 +177,20 @@ public class Dlist{
     }
   }//end of set
 
-  // insert an item before index.
-  // only inserts if the index is within bounds
+  /**
+  * Inserts a node at a given index.
+  * Checks to see if the index is in the half of the dlist closer to head or tail.
+  * If the given index is closer to tail, method starts at tail and traverses in reverse to find the index.
+  * If the given index is closer to head, method starts at head and traverses forward to find the index.
+  * Once the index is found, the method creates a new node using the data passed as a parameter.
+  * The new node is then inserted into the dlist before the node at index.
+  * If the index is not in bounds, will throw a NullPointerException.
+  *
+  * @param index - the index at which to insert the new node.
+  * @param value - the data that the new node will contain.
+  *
+  * @return void
+  */
   public void insert(int index, String value){
     if (index > size || index < 0){
       throw new NullPointerException("Can't insert there!");
