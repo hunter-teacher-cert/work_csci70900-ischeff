875844bc0d6054dedeb8453915e9f104721f8759
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-08-06 01:48:32 -0400

    adding prompts

---
+++ meth1/lessonplans/lesson2/MethodsMILD.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class MethodsMILD{
+
+  public static String euroDate(//your parameters go here){
+    //start with an empty string
+    String result = "";
+    //then assign the string to the concatenated format!
+    result = ;
+    return result;
+  }
+
+  public static String amDate(String month, String day, int date, int year){
+    //start with an empty string
+    String result = "";
+    //then assign the string to the concatenated format!
+    result = ;
+    return result;
+  }
+
+  public static void main(String[] args){
+    //Add parameters to test your code here!
+    System.out.println(euroDate());
+    System.out.println(amDate());
+
+  }
+}
+
+//Done early? Try medium! 
