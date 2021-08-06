35b1bc3653af1614ec2aa70e37b693513dfb70af
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-08-06 02:21:29 -0400

    added link to javadoc for scanner

--- meth1/lessonplans/lesson2/MethodsSPICY.java
+++ meth1/lessonplans/lesson2/MethodsSPICY.java
@@ -3,10 +3,12 @@ import java.util.*;
 
 public class MethodsSPICY{
   //create methods (one per parameter) to solicit user input
+    //to get user input, you need to create a Scanner
+    //for more on scanners, check this link: https://docs.oracle.com/en/java/javase/16/docs/api/java.base/java/util/Scanner.html
 
   //create a method called date that takes as parameters the day, month, date, year, and date format and returns the date in that format.
 
   //create a main method to print out today's date
     //main should invoke your date method and ALSO the other methods
-      //the other methods should pass the user data to date to print the result! 
+      //the other methods should pass the user data to date to print the result!
 }
