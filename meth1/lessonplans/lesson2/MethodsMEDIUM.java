a26f86ebf2d6e0e7722bab38d9643c76bd047b7c
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-08-06 02:17:43 -0400

    Added frame

--- meth1/lessonplans/lesson2/MethodsMEDIUM.java
+++ meth1/lessonplans/lesson2/MethodsMEDIUM.java
@@ -2,5 +2,17 @@ import java.io.*;
 import java.util.*;
 
 public class MethodsMEDIUM{
-  
+
+  //this method should take as parameters the day, date, month, year, and date format.
+  public static String date(){
+    String result = "";
+    //add code here to assign the empty string to a concatenation of the parameters!
+    return result;
+  }
+
+  public static void main(String[] args){
+    //add parameters to the lines below to test print today's date in BOTH european and american format! 
+    System.out.println(date());
+    System.out.println(date());
+  }
 }
