f0cf317f8df92c52ef25c3887bdfd882d1d91c13
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-08-06 02:20:23 -0400

    added prompts

--- meth1/lessonplans/lesson2/MethodsSPICY.java
+++ meth1/lessonplans/lesson2/MethodsSPICY.java
@@ -2,5 +2,11 @@ import java.io.*;
 import java.util.*;
 
 public class MethodsSPICY{
-  
+  //create methods (one per parameter) to solicit user input
+
+  //create a method called date that takes as parameters the day, month, date, year, and date format and returns the date in that format.
+
+  //create a main method to print out today's date
+    //main should invoke your date method and ALSO the other methods
+      //the other methods should pass the user data to date to print the result! 
 }
